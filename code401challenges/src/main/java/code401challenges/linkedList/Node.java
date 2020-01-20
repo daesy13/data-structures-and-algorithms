dded1d3f8c6a140365af121b20cba434be5470b2
daesy <daesyramos@hotmail.com>
2020-01-20 13:45:09 -0800

    all methods added

--- code401challenges/src/main/java/code401challenges/linkedList/Node.java
+++ code401challenges/src/main/java/code401challenges/linkedList/Node.java
@@ -1,4 +1,4 @@
-package code401challenges;
+package code401challenges.linkedList;
 
 public class Node {
     // this are instance variables
--- code401challenges/src/test/java/code401challenges/linkedList/LinkedListTest.java
+++ code401challenges/src/test/java/code401challenges/linkedList/LinkedListTest.java
@@ -24,23 +24,31 @@ public class LinkedListTest {
         actual.insert(40);
 
         String expected = "{40} -> {30} -> {20} -> NULL";
-        assertTrue(expected.equals(expected));
+        assertTrue(expected.equals(actual.toString()));
     }
 
     @Test public void testAppend(){
         LinkedList actual = new LinkedList();
-        actual.append(1);
-        actual.append(3);
-        actual.append(2);
+        actual.insert(2);
+        actual.insert(3);
+        actual.insert(1);
+        actual.append(5);
         String expected = "{1} -> {3} -> {2} -> {5} -> NULL";
-        assertEquals(expected, actual.toString());
+        assertTrue(expected.equals(actual.toString()));
+    }
+
+    @Test public void testAppendOne(){
+        LinkedList actual = new LinkedList();
+        actual.append(5);
+        String expected = "{5} -> NULL";
+        assertTrue(expected.equals(actual.toString()));
     }
 
     @Test public void testInsertBefore(){
         LinkedList actual = new LinkedList();
-        actual.append(1);
-        actual.append(3);
-        actual.append(2);
+        actual.insert(2);
+        actual.insert(3);
+        actual.insert(1);
         actual.insertBefore(3,5);
         String expected = "{1} -> {5} -> {3} -> {2} -> NULL";
         assertEquals(expected, actual.toString());
@@ -48,12 +56,24 @@ public class LinkedListTest {
 
     @Test public void testInsertAfter(){
         LinkedList actual = new LinkedList();
-        actual.append(1);
-        actual.append(3);
-        actual.append(2);
+        actual.insert(2);
+        actual.insert(3);
+        actual.insert(1);
         actual.insertAfter(3,5);
         String expected = "{1} -> {3} -> {5} -> {2} -> NULL";
         assertEquals(expected, actual.toString());
+    }
+
+    @Test public void testKFromEnd(){
+        LinkedList actual = new LinkedList();
+        actual.insert(2);
+        actual.insert(8);
+        actual.insert(3);
+        actual.insert(1);
+        int pending = actual.kthFromEnd(0);
+        int expected = 2;
+        assertEquals(expected, 2);
+
     }
 
 }
