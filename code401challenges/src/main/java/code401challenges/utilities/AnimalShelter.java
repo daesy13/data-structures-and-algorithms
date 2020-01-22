564a1a2beeafb6bfd8bd9a137ec8a189c2329c9c
daesy <daesyramos@hotmail.com>
2020-01-22 01:00:32 -0800

    some dogs and cats

---
+++ code401challenges/src/main/java/code401challenges/utilities/AnimalShelter.java
@@ -0,0 +1,19 @@
+package code401challenges.utilities;
+
+import code401challenges.stacksandqueues.Stack;
+
+public class AnimalShelter {
+    public Stack catsStack;
+    public Stack dogsStack;
+
+    public AnimalShelter() {
+        catsStack = new Stack();
+        dogsStack = new Stack();
+    }
+
+    public void enqueue(String animal){
+       
+    }
+
+}
+
