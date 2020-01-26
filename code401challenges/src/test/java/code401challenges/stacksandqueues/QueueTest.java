6303b71007b715976470a9a1b5e6db53aab40e9b
daesy <daesyramos@hotmail.com>
2020-01-26 00:45:37 -0800

    Stack test done

--- code401challenges/src/test/java/code401challenges/stacksandqueues/QueueTest.java
+++ code401challenges/src/test/java/code401challenges/stacksandqueues/QueueTest.java
@@ -1,4 +1,9 @@
 package code401challenges.stacksandqueues;
-
+//Can successfully enqueue into a queue
+//Can successfully enqueue multiple values into a queue
+//Can successfully dequeue out of a queue the expected value
+//Can successfully peek into a queue, seeing the expected value
+//Can successfully empty a queue after multiple dequeues
+//Can successfully instantiate an empty queue
 public class QueueTest {
 }
