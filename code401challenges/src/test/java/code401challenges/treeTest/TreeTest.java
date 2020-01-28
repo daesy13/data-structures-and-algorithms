6aa0ea76c34b093dbbe2b14fe021256789c49a3a
daesy <daesyramos@hotmail.com>
2020-01-28 14:12:08 -0800

    fixed commit

--- code401challenges/src/test/java/code401challenges/treeTest/TreeTest.java
+++ code401challenges/src/test/java/code401challenges/treeTest/TreeTest.java
@@ -5,13 +5,9 @@ import code401challenges.tree.Tree;
 import org.junit.Test;
 
 public class TreeTest {
-<<<<<<< HEAD
     @Test
     public void constructorTest(){
         Node root = new Node(1, new Node(2), new Node (3));
         Tree tree = new Tree(root);
     }
-=======
-
->>>>>>> 429b11afa41bb6bb696662b2fb643785dcf6a9ae
 }
