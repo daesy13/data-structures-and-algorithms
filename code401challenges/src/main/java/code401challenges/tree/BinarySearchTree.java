8df7a6b8307e03cf204752dee333420820fdf5ec
daesy <daesyramos@hotmail.com>
2020-01-27 23:40:49 -0800

    tree lab

---
+++ code401challenges/src/main/java/code401challenges/tree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package code401challenges.tree;
+
+import java.util.ArrayList;
+
+// Create a BinarySearchTree class
+// Define a method named add that accepts a value, and adds a new node with that value in the correct location in the
+// binary search tree.
+// Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is
+// in the tree at least once.
+public class BinarySearchTree {
+
+}
---
+++ code401challenges/src/main/java/code401challenges/tree/Tree.java
@@ -0,0 +1,76 @@
+package code401challenges.tree;
+
+import java.util.ArrayList;
+
+public class Tree {
+    public Node root;
+
+    public Tree(Node root){
+        this.root = root;
+    }
+
+    // method overloading
+    public Tree(){
+        this.root = null;
+    }
+
+    // INORDER: Left, Root, Right
+    private ArrayList<Integer> inOrder(Node node){
+        if (node == null){
+            return new ArrayList<>();
+        }
+        else {
+            ArrayList<Integer> resultInOrder = new ArrayList<>();
+
+            ArrayList<Integer> left = inOrder(node.left);
+            resultInOrder.addAll(left);
+
+            resultInOrder.add(node.value);
+
+            ArrayList<Integer> right = inOrder(node.right);
+            resultInOrder.addAll(right);
+
+            return resultInOrder;
+        }
+    }
+
+
+    // PREORDER: Root, Left, Right
+    private ArrayList<Integer> preOrder(Node node){
+        if (node == null){
+            return new ArrayList<>();
+        }
+        else {
+            ArrayList<Integer> result = new ArrayList<>();
+            result.add(node.value);
+
+            ArrayList<Integer> left = preOrder(node.left);
+            result.addAll(left);
+
+            ArrayList<Integer> right = preOrder(node.right);
+            result.addAll(right);
+
+            return result;
+        }
+    }
+
+    // POSTORDER: Left, Right, Root
+    private ArrayList<Integer> postOrder(Node node){
+        if (node == null){
+            return new ArrayList<>();
+        }
+        else {
+            ArrayList<Integer> resultPostOrder = new ArrayList<>();
+
+            ArrayList<Integer> left = postOrder(node.left);
+            resultPostOrder.addAll(left);
+
+            ArrayList<Integer> right = postOrder(node.right);
+            resultPostOrder.addAll(right);
+
+            resultPostOrder.add(node.value);
+
+            return resultPostOrder;
+        }
+    }
+}
