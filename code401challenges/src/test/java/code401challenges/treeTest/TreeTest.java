5182afa90c9ee3a4c855f1e3a4c1ddba47403cd6
daesy <daesyramos@hotmail.com>
2020-02-01 13:12:10 -0800

    more test

--- code401challenges/src/test/java/code401challenges/treeTest/TreeTest.java
+++ code401challenges/src/test/java/code401challenges/treeTest/TreeTest.java
@@ -5,6 +5,7 @@ import code401challenges.tree.Tree;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TreeTest {
     @Test
@@ -14,6 +15,7 @@ public class TreeTest {
     }
 
     // maxValueTest
+    // Happy Test
     @Test
     public void maxValueTest(){
         Node root = new Node(1, new Node(2), new Node (3));
@@ -21,4 +23,16 @@ public class TreeTest {
         int actual = tree.findMaximumValue();
         assertEquals(3,actual);
     }
+
+    //Bad Result
+    @Test
+    public void maxValueWrongValueTest(){
+        Node root = new Node(1, new Node(2), new Node (3));
+        Tree tree = new Tree(root);
+        int actual = tree.findMaximumValue();
+        assertNotEquals(4,actual);
+    }
+
+
+
 }
