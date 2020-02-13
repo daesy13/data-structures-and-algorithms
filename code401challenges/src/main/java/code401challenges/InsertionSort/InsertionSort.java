a08b84720440078c025c2e7b0be02b6f85c0447e
daesy <daesyramos@hotmail.com>
2020-02-13 13:17:49 -0800

    test passed

--- code401challenges/src/main/java/code401challenges/InsertionSort/InsertionSort.java
+++ code401challenges/src/main/java/code401challenges/InsertionSort/InsertionSort.java
@@ -1,18 +1,33 @@
 package code401challenges.InsertionSort;
 public class InsertionSort {
-    public static void insertionSort(int []arr){
+    public static int[] insertionSort(int []arr){
         if(arr.length == 0){
             throw new IllegalArgumentException("This array is empty, not able to perform with empty arrays");
         }
-        for (int i=0; i<arr.length; i++){
-            for (int j=1; j<arr.length-1; j++){
-                if (arr[j]> arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+        for (int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            int j=i-1;
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j=j-1;
             }
-        }
+            arr[j + 1] = temp;
+            }
+        return arr;
     }
-    // Ref.:https://www.geeksforgeeks.org/bubble-sort/
 }
+    // Ref.:https://www.geeksforgeeks.org/bubble-sort/
+
+
+// InsertionSort(int[] arr)
+//
+//    FOR i = 1 to arr.length
+//
+//      int j <-- i - 1
+//      int temp <-- arr[i]
+//
+//      WHILE j >= 0 AND temp < arr[j]
+//        arr[j + 1] <-- arr[j]
+//        j <-- j - 1
+//
+//      arr[j + 1] <-- temp
--- code401challenges/src/test/java/code401challenges/InsertionSortTest/InsertionSortTest.java
+++ code401challenges/src/test/java/code401challenges/InsertionSortTest/InsertionSortTest.java
@@ -3,10 +3,42 @@ package code401challenges.InsertionSortTest;
 import code401challenges.InsertionSort.InsertionSort;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static code401challenges.InsertionSort.InsertionSort.insertionSort;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class InsertionSortTest {
 
+    // HAPPY TEST
     @Test
-    public void insertArray(){
+    public void insertionSortTest(){
+        int[] input = {9,5,4,11,25,45};
+        int[] actual = insertionSort(input);
+        int[] expected = {4, 5, 9, 11, 25, 45};
+//        System.out.println("actual = " + Arrays.toString(actual));
+        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+    }
 
+    // FAIL TEST
+    @Test
+    public void insertionFailTest(){
+        int[] input = {9,5,4,11,25,45};
+        int[] actual = insertionSort(input);
+        int[] expected = {9,5,4,11,25,45};
+//        System.out.println("actual = " + Arrays.toString(actual));
+        assertNotEquals(Arrays.toString(expected), Arrays.toString(actual));
+    }
+
+    // EDGE CASE
+    @Test
+    public void insertionEdgeTest(){
+        int[] input = {};
+        int[] actual = insertionSort(input);
+        System.out.println("actual = " + actual);
+//        int[] expected = {9,5,4,11,25,45};
+//        System.out.println("actual = " + Arrays.toString(actual));
+        assertNotEquals("This array is empty, not able to perform with empty arrays\n", actual);
     }
 }
