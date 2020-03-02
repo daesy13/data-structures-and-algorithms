8d13e61bbe2219f53147a22e2093e6f899d87ea0
daesy <daesyramos@hotmail.com>
2020-03-02 13:07:08 -0800

    code ch35 done

--- code401challenges/src/main/java/code401challenges/graph/Graph.java
+++ code401challenges/src/main/java/code401challenges/graph/Graph.java
@@ -15,15 +15,18 @@ public class Graph<E> {
 //Adds a new node to the graph
 //Takes in the value of that node
 //Returns the added node
-    public void addNode(E value){
+    public Vertex addNode(E value){
         if (value != null){
             Vertex newNode;
             newNode = new Vertex(value);
             vertices.putIfAbsent(newNode, new HashSet<>());
+            return newNode;
+        } else {
+            throw new IllegalArgumentException("no empty values please");
         }
     }
 
-    public void addEdge(Vertex from, Vertex to, int weight){
+    public Edge addEdge(Vertex from, Vertex to, int weight){
         if (vertices.containsKey(from) && vertices.containsKey(to)) {
             // create an edge "from" to "to"
             Edge fromToEdge = new Edge<Vertex>(from, to, weight);
@@ -34,7 +37,10 @@ public class Graph<E> {
             Edge toFromEdge = new Edge<Vertex>(to, from, weight);
             Set<Edge> getToNeighbor = vertices.get(to);
             getToNeighbor.add(toFromEdge);
+
+            return fromToEdge;
         }
+        throw new IllegalArgumentException("no empty values please");
     }
 
     public Set<Vertex> getNodes(){
